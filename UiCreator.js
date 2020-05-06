cb9e5d572203ea16889b6829ab4b0388e26b09d7
hazemkhairy <hazemkhairy188@gmail.com>
2020-05-06 08:48:07 +0200

    -Added jumperton + Carousel -Added dynamic card generation integreted with backend -Added popover + buttongroup for each card -Added Pagination -Added Breadcrumb -Added Accordion Collapse as footer

---
+++ UiCreator.js
@@ -0,0 +1,149 @@
+const init = () => {
+    $('.carousel').carousel()
+    $('[data-toggle="popover"]').popover({
+        html: true,
+        template: '<div class="popover bg-white rounded" role="tooltip"><div class="arrow"></div><h3 class="popover-header bg-white border-bottom"></h3><div class="popover-body p-0"></div></div>'
+    }).on('shown.bs.popover', function () {
+        setTimeout(function (a) {
+            a.popover('hide');
+        }, 3000, $(this));
+    });
+}
+
+const createRoundButton = (text) => {
+    const div = document.createElement('div');
+    const button = document.createElement('button');
+    button.classList.add('btn', 'btn-light', 'rounded-pill', 'border-0', 'p-1');
+    const p = document.createElement('p');
+    p.classList.add('p-0', 'm-1', 'align-center')
+    p.innerHTML = text;
+    p.style.fontSize = '125%'
+    p.style.fontWeight = "600"
+    button.appendChild(p)
+    div.appendChild(button);
+    return div;
+}
+const createPopOverEffect = (optionsButton) => {
+
+    const btnGroup = document.createElement('div')
+    btnGroup.classList.add('btn-group-vertical', 'w-100', 'btn-light', 'p-0');
+
+    const btnsContent = ['Hide Pin', 'Download image', 'Report Pin'];
+    for (let i = 0; i < btnsContent.length; i++) {
+        let temp = document.createElement('button');
+        temp.classList.add('btn', 'btn-light', 'w-100', 'text-left');
+        temp.innerText = btnsContent[i];
+        btnGroup.appendChild(temp)
+    }
+
+    optionsButton.title = 'This Pin was inspired by your recent activity'
+    optionsButton.setAttribute('data-toggle', 'button')
+    optionsButton.setAttribute('data-toggle', 'popover')
+    optionsButton.setAttribute('data-content', btnGroup.outerHTML)
+    return optionsButton
+}
+const createLowerRow = () => {
+    const lowerRow = document.createElement('div');
+    lowerRow.classList.add('d-flex', 'flex-row-reverse', 'justify-content-around');
+    let optionsButton = createRoundButton('&#8230');
+    optionsButton = createPopOverEffect(optionsButton)
+    const shareButton = createRoundButton('sh');
+    const handleButton = createRoundButton('handle');
+
+    lowerRow.appendChild(optionsButton);
+    lowerRow.appendChild(shareButton);
+    lowerRow.appendChild(handleButton);
+    return lowerRow;
+}
+const createUpperRow = () => {
+    const upperRow = document.createElement('div');
+    upperRow.classList.add('d-flex', 'flex-row-reverse');
+
+    const saveButton = document.createElement('button');
+    saveButton.classList.add('rounded-pill', 'bg-danger', 'border-0', 'text-white', 'p-2')
+    saveButton.textContent = "Save"
+    saveButton.style.fontWeight = "600"
+    upperRow.appendChild(saveButton);
+    return upperRow;
+}
+const createCoverCardDecoration = () => {
+
+    const decoration = document.createElement('div');
+
+    decoration.style.height = "100%"
+    decoration.style.width = "100%"
+    decoration.classList.add('d-flex', 'flex-column', 'justify-content-between', 'p-2')
+
+    const upperRow = createUpperRow();
+    const lowerRow = createLowerRow();
+
+
+    decoration.appendChild(upperRow);
+    decoration.appendChild(lowerRow);
+    return decoration;
+}
+
+const createCardCover = () => {
+    const cardCover = document.createElement('div');
+    cardCover.style.backgroundColor = 'rgba(125,125,125,0.3)'
+    cardCover.style.visibility = 'hidden'
+    cardCover.style.height = "100%"
+    cardCover.style.width = "100%"
+    cardCover.style.position = "absolute"
+    const coverCardDecoration = createCoverCardDecoration()
+    cardCover.appendChild(coverCardDecoration);
+    return cardCover
+
+}
+
+const createCard = (data) => {
+    const card = document.createElement('div');
+    card.classList.add('card', 'col-5', 'col-sm-4', 'col-md-2', 'm-2', 'p-0','modifiedCard')
+
+
+    const cardCover = createCardCover();
+    card.onmouseenter = () => { cardCover.style.visibility = 'visible' }
+    card.onmouseleave = () => { cardCover.style.visibility = 'hidden' }
+
+    const img = document.createElement('img')
+    img.src = data.url;
+    img.classList.add('img');
+    img.style.maxHeight='350px'
+    
+    card.appendChild(cardCover)
+    card.appendChild(img)
+    return card;
+
+}
+
+const createCardsArray = async (data) => {
+    const res = await data.map(d => {
+        return createCard(d);
+    })
+    return res;
+}
+
+const displayPhotos = (photos) => {
+
+    const view = document.getElementById('photosGrid');
+    view.innerHTML='';
+    photos.forEach(element => {
+        view.append(element)
+
+    });
+}
+const getDivsOfMostPopular = (photos) => {
+    const divs = [];
+    for (let i = 0; i < photos.length; i++) {
+        const container = document.createElement('div');
+        container.classList.add('carousel-item', 'd-flex', 'align-items-center');
+        const img = document.createElement('img');
+        img.classList.add('img-fluid', 'd-block', 'w-100');
+        img.src = photos[i].url;
+        img.style.maxHeight = '350px'
+        container.appendChild(img);
+        divs.push(container);
+    }
+    divs[0].classList.add('active');
+    return divs;
+}
---
+++ index.js
@@ -0,0 +1,80 @@
+
+
+const getPhotos = (limit = 5) => {
+    const ret = [];
+
+    for (let i = 0; i < limit; i++) {
+        let temp = { url: 'https://robohash.org/' + Math.random() * 10000, name: i.toString() };
+        ret.push(temp);
+    }
+
+    return ret;
+}
+const getPhotos2 = (posts) => {
+    const ret = [];
+
+    for (let i = 0; i < posts.length; i++) {
+        let temp = { ...posts[i], url: posts[i].picture };
+        ret.push(temp);
+    }
+
+    return ret;
+}
+const displayMostPopular = () => {
+
+    const view = document.getElementById('mostPopular');
+    const photos = getPhotos(5)
+    getDivsOfMostPopular(photos).forEach(element => {
+        view.append(element)
+
+    });
+    init();
+}
+
+const loadPhotos = async () => {
+    const photos = getPhotos(20);
+    const cards = await createCardsArray(photos);
+    displayPhotos(cards);
+    displayMostPopular();
+    init();
+}
+loadPhotos();
+
+
+const paginationCreator = async (curPage) => {
+    let payload = await fetch(`https://pinterest-clone-restful-api.herokuapp.com/api/Posts/getAllPosts/{curPage}/`)
+        .then(
+            (res) => {
+                return res.json();
+            }
+        )
+    payload = payload.payload
+    console.log(payload)
+    let maxPages = payload.pages;
+    let paginationDiv = document.getElementById('pagination');
+    paginationDiv.innerHTML = '';
+    for (let i = 1; i <= maxPages; i++) {
+        let li = document.createElement('li');
+        li.classList.add('page-item');
+        if (i == curPage) {
+            li.classList.add('active')
+            let span = document.createElement('span');
+            span.classList.add('page-link');
+            span.innerText = curPage;
+            li.appendChild(span);
+        } else {
+            let button = document.createElement('button');
+            button.classList.add('page-link');
+            button.innerText = i;
+            button.onmousedown = () => { paginationCreator(i) }
+            li.appendChild(button);
+        }
+        console.log(li)
+        paginationDiv.appendChild(li);
+    }
+    let photos = getPhotos2(payload.posts);
+    const cards = await createCardsArray(photos);
+    displayPhotos(cards);
+    init();
+}
+paginationCreator(1);
