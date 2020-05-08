6e3866214d67f8aeed3bbc905b663e5d817a4e67
hazemkhairy <hazemkhairy188@gmail.com>
2020-05-08 10:26:20 +0200

    -Modified html to be responsive -Modified html to use bootstrap more than css -Refactored some  codes in js -Removed some unneeded code from javascript -Changed 'var' to 'let' in javascript

--- Modals/modal.js
+++ Modals/modal.js
@@ -1,207 +1,181 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $("#preAuthentication").modal('show');
 });
 
-const validateLoginForm=()=> {
-var email  = document.getElementById("loginemail").value;
-var password= document.getElementById("loginpassword").value;
-var valid = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-var invalid=false;
+const validateLoginForm = () => {
+  let email = document.getElementById("loginemail").value;
+  let password = document.getElementById("loginpassword").value;
+  let valid = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+  let invalid = false;
 
-if (email == "" ||password == ""){
-invalid=true;
-$(".successAlert").hide();
-$(".errorAlert").show();
-setTimeout(function(){
-  $(".errorAlert").hide(); 
-}, 5000);
+  if (email == "" || password == "") {
+    invalid = true;
+    $(".successAlert").hide();
+    $(".errorAlert").show();
+    setTimeout(function () {
+      $(".errorAlert").hide();
+    }, 5000);
 
-return false;
+    return false;
+  }
+  if (valid.test(email) !== true) {
+    invalid = true;
+    $(".invalidemailAlert").show();
+    setTimeout(function () {
+      $(".invalidemailAlert").hide();
+    }, 5000);
+    return false
+  }
+  return true;
 }
-if (valid.test(email) !== true) {
-invalid=true;
-$(".invalidemailAlert").show();
-setTimeout(function(){
-  $(".invalidemailAlert").hide(); 
-}, 5000);
-return false
-}
-return true;
-}
-const Login=()=>
-{
-var email  = document.getElementById("loginemail").value.toString();
-var password= document.getElementById("loginpassword").value.toString();
-if(validateLoginForm()==true)
-{
-fetch(`https://pinterest-clone-restful-api.herokuapp.com/api/User/Login`,
-  {
-    method:'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body:JSON.stringify({
+const Login = () => {
+  let email = document.getElementById("loginemail").value.toString();
+  let password = document.getElementById("loginpassword").value.toString();
+  if (validateLoginForm() == true) {
+    fetch(`https://pinterest-clone-restful-api.herokuapp.com/api/User/Login`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
 
-        email:email,
-        password:password
-      
-    })
-  }).then(response => response.json())
-  .then(data => {
-    console.log('Success:', data);
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+          email: email,
+          password: password
+
+        })
+      }).then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
 }
+
+const showSignUpErrorAlert = (message) => {
+  let errorMessage = document.getElementById('errorMessage')
+  errorMessage.innerText = message;
+  $(".successAlert").hide();
+  $(".errorAlert").show();
+  setTimeout(function () {
+    $(".errorAlert").hide();
+  }, 5000);
 }
 function validateSignUpForm() {
-  var email  = document.getElementById("email").value;
-  var password= document.getElementById("password").value;
-  var name = document.getElementById("name").value;
-  var birthdate = document.getElementById("birthdate").value;
-  var profilePicUrl = document.getElementById("profilePicUrl").value;
-  var invalid=false
-  var valid = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-      if (email == "" || password == "" || name=="" || birthdate=="" || profilePicUrl=="")
-      {
-         invalid=true
-         $(".successAlert").hide();
-         $(".errorAlert").show();
-         setTimeout(function(){
-              $(".errorAlert").hide(); 
-         }, 5000);
-         return false;
-      }
-      else
-      {
-      if (valid.test(email) !== true) {
-        invalid=true;
-        $(".invalidemailAlert").show();
-        setTimeout(function(){
-          $(".invalidemailAlert").hide(); 
-        }, 5000);
-        return false;
-      }
-      if(password.length<8)
-      {
-        invalid=true;
-        $(".shortpasswordAlert").show();
-        setTimeout(function(){
-          $(".shortpasswordAlert").hide(); 
-        }, 5000);
-        return false;
-      }
-     
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  let name = document.getElementById("name").value;
+  let birthdate = document.getElementById("birthdate").value;
+  let profilePicUrl = document.getElementById("profilePicUrl").value;
+  let valid = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+  if (email == "" || password == "" || name == "" || birthdate == "" || profilePicUrl == "") {
+    showSignUpErrorAlert('Please Fill required data.')
+    return false;
+  }
+  else {
+    if (valid.test(email) !== true) {
+      showSignUpErrorAlert('Invalid Email')
+      return false;
     }
-    return true;
-   
+    if (password.length < 8) {
+      showSignUpErrorAlert('Short Password')
+      return false;
+    }
+  }
+  return true;
+
 }
-const SignUp=()=> {
-  var email  = document.getElementById("email").value.toString();
-  var password= document.getElementById("password").value.toString();
-  var name = document.getElementById("name").value.toString();
-  var birthdate = document.getElementById("birthdate").value.toString();
-  var birthdate = birthdate+'T14:48:00.000Z'
-  var profilePicUrl = document.getElementById("profilePicUrl").value.toString();
- validateSignUpForm();
-if(validateSignUpForm()==true)
-{
-  fetch(`https://pinterest-clone-restful-api.herokuapp.com/api/User/Register`,
-  {
-    method:'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body:JSON.stringify({
-      name:name,
-      birthDate:birthdate,
-      email:email,
-      password:password,
-      profilePicture:profilePicUrl
-    })
-   
-    
-  }).then(response => response.json())
-  .then(data => {
-    console.log('Success:', data);
-    $(".errorAlert").hide();
-    $(".successAlert").show();
-    setTimeout(function(){
-      $(".successAlert").hide(); 
-    }, 5000);
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
-  
-}
+const SignUp = () => {
+  let email = document.getElementById("email").value.toString();
+  let password = document.getElementById("password").value.toString();
+  let name = document.getElementById("name").value.toString();
+  let birthdate = document.getElementById("birthdate").value.toString();
+  birthdate = birthdate + 'T14:48:00.000Z'
+  let profilePicUrl = document.getElementById("profilePicUrl").value.toString();
+  let obj = {
+    name: name,
+    birthDate: birthdate,
+    email: email,
+    password: password,
+    profilePicture: profilePicUrl
+  }
+  if (validateSignUpForm() == true) {
+    fetch(`https://pinterest-clone-restful-api.herokuapp.com/api/User/Register`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(obj)
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+        $(".errorAlert").hide();
+        $(".successAlert").show();
+        setTimeout(function () {
+          $(".successAlert").hide();
+        }, 5000);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        showSignUpErrorAlert(error)
+      });
+
+  }
 }
 
 $(function () {
-$('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="tooltip"]').tooltip()
 })
-$(document).ready(function(){
-$('[data-toggle="popover"]').popover(); 
+$(document).ready(function () {
+  $('[data-toggle="popover"]').popover();
 });
 
-  // Resposive Modal
-  // $('.modal-content').resizable({
-  //       //alsoResize: ".modal-dialog",
-  //       minHeight: 300,
-  //       minWidth: 300
-  //     });
-  // $('.modal-dialog').draggable();
 
-  // $('#myModal').on('show.bs.modal', function() {
-  //   $(this).find('.modal-body').css({
-  //     'max-height': '100%'
-  //   });
-  // });
- 
- 
- 
-  function isGood(password) {
-    var password_strength = document.getElementById("password-text");
+function isGood(password) {
+  let password_strength = document.getElementById("password-text");
 
-    //TextBox left blank.
-    if (password.length == 0) {
-      password_strength.innerHTML = "";
-      return;
+  //TextBox left blank.
+  if (password.length == 0) {
+    password_strength.innerHTML = "";
+    return;
+  }
+
+  //Regular Expressions.
+  let regex = new Array();
+  regex.push("[A-Z]"); //Uppercase Alphabet.
+  regex.push("[a-z]"); //Lowercase Alphabet.
+  regex.push("[0-9]"); //Digit.
+  regex.push("[$@$!%*#?&]"); //Special Character.
+
+  let passed = 0;
+
+  //Validate for each Regular Expression.
+  for (let i = 0; i < regex.length; i++) {
+    if (new RegExp(regex[i]).test(password)) {
+      passed++;
     }
+  }
 
-    //Regular Expressions.
-    var regex = new Array();
-    regex.push("[A-Z]"); //Uppercase Alphabet.
-    regex.push("[a-z]"); //Lowercase Alphabet.
-    regex.push("[0-9]"); //Digit.
-    regex.push("[$@$!%*#?&]"); //Special Character.
-
-    var passed = 0;
-
-    //Validate for each Regular Expression.
-    for (var i = 0; i < regex.length; i++) {
-      if (new RegExp(regex[i]).test(password)) {
-        passed++;
-      }
-    }
-
-    //Display status.
-    var strength = "";
-    switch (passed) {
-      case 0:
-      case 1:
-      case 2:
-        strength = "<small class='progress-bar bg-danger' role='progressbar' style='width: 20%'>Weak</small>";
-        break;
-      case 3:
-        strength = "<small class='progress-bar bg-warning' role='progressbar' style='width: 40%'>Medium</small>";
-        break;
-      case 4:
-        strength = "<small class='progress-bar bg-success' role='progressbar' style='width: 60%'>Strong</small>";
-        break;
-
-    }
-    password_strength.innerHTML = strength;
+  //Display status.
+  let strength = "";
+  switch (passed) {
+    case 0:
+    case 1:
+    case 2:
+      strength = "<small class='progress-bar bg-danger' role='progressbar' style='width: 20%'>Weak</small>";
+      break;
+    case 3:
+      strength = "<small class='progress-bar bg-warning' role='progressbar' style='width: 40%'>Medium</small>";
+      break;
+    case 4:
+      strength = "<small class='progress-bar bg-success' role='progressbar' style='width: 60%'>Strong</small>";
+      break;
 
   }
+  password_strength.innerHTML = strength;
+
+}
